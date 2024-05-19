f06d8059227d13a809d504013cba208024a41437
Tmothiba <mothibatshwarelo99@gmail.com>
2024-05-19 12:35:42 -0700

    a class BaseModel that defines all common attributes/methods for other classes

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-
+BaseModel: base.py
 """
 import uuid
 from datetime import datetime
@@ -8,7 +8,14 @@ from datetime import datetime
 
 
 class BaseModel:
+    """
+    Base class which defines all common
+    attributes/methods for other classes
+    """
     def __init__(self):
+        """
+        instatiates an object with it's attributes
+        """
         self.id = str(uuid.uuid4())
 
         self.created_at = datetime.now()
@@ -16,13 +23,13 @@ class BaseModel:
 
     def save(self):
         """
-
+        updates the public instance attribute updated_at with the current datetime
         """
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """
-
+        returns a dictionary containing all keys/values of __dict__ of the instance
         """
         inst_dict = self.__dict__.copy()
         inst_dict["__class__"] = self.__class__.__name__
