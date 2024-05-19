00cc62276978c2976576936b2a506cd4236cba79
Tmothiba <mothibatshwarelo99@gmail.com>
2024-05-19 13:09:42 -0700

    re-create an instance with this dictionary representation

--- models/base_model.py
+++ models/base_model.py
@@ -12,14 +12,28 @@ class BaseModel:
     Base class which defines all common
     attributes/methods for other classes
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
-        instatiates an object with it's attributes
+        instatiates an object with it's attributes. The constructor
         """
-        self.id = str(uuid.uuid4())
+        if kwargs:
+        # self.__dict__ = kwargs
+        # self.created_at = datetime.strptime(self.created_at,
+        #                                     "%Y-%m-%dT%H:%M:%S.%f")
 
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        # self.updated_at = datetime.strptime(self.updated_at,
+        #                                     "%Y-%m-%dT%H:%M:%S.%f")
+            for key, value in kwargs.items():
+                if key == "created_at":
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key == "updated_at":
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """
